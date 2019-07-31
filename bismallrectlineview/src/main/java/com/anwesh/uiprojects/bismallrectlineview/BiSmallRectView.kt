552bf66dc747e63004ac79f8380bf099e42fc95f
Anwesh43 <anweshthecool0@gmail.com>
2019-07-31 17:56:29 +0530

    addin Int and Float extension methods

--- bismallrectlineview/src/main/java/com/anwesh/uiprojects/bismallrectlineview/BiSmallRectView.kt
+++ bismallrectlineview/src/main/java/com/anwesh/uiprojects/bismallrectlineview/BiSmallRectView.kt
@@ -3,3 +3,22 @@ package com.anwesh.uiprojects.bismallrectlineview
 /**
  * Created by anweshmishra on 31/07/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Canvas
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 2
+val parts : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#388E3C")
+val backColor : Int = Color.parseColor("#BDBDBD")
